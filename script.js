63bbc7b3e19070d5694ab589c252284092e81ae6
danihire@gmail.com <danihire@gmail.com>
2022-06-04 14:37:28 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+const dateSelector = document.getElementById('date-selector');
+let dates = document.getElementById("date");
+let spaces = document.getElementById("spaces");
+const form = document.querySelector("form");
+let space;
+let arr = Array();
+
+fetch("http://127.0.0.1:8000/api/space/")
+.then(response => response.json())
+.then(data => {
+  data.forEach(d => {
+    let opt = document.createElement("option");
+    opt.value = d.id;
+    opt.textContent = d.space_name;
+    spaces.appendChild(opt);
+  })
+});
+
+dateSelector.addEventListener('change', e => {
+  arr.push(e.target.value);
+  dates.value = arr;
+});
