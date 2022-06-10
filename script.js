0f0daf5dbe30e3d08d592c1621e68f9886ea7828
danihire@gmail.com <danihire@gmail.com>
2022-06-10 03:31:31 +0100

    hourly booking

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ let dates = document.getElementById("date");
 let spaces = document.getElementById("spaces");
 const form = document.querySelector("form");
 const spaceTypes = document.getElementById("space-types");
+const bookingTypes = document.getElementById("booking-types");
+const startTime = document.getElementById("start-time");
 
 let arr = Array();
 let dateSearch;
@@ -10,10 +12,9 @@ let dateSearch;
 // const endpoint = "http://127.0.0.1:8000/api/available/";
 // const endpoint = "https://workspaceapi.herokuapp.com/api/available/";
 
-fetch("https://workspaceapi.herokuapp.com/api/space-type/")
+fetch("http://127.0.0.1:8000/api/space-type/")
 .then(response => response.json())
 .then(data => {
-  console.log(data);
   data.forEach(d => {
     let opt = document.createElement("option");
     opt.value = d.id;
@@ -22,10 +23,46 @@ fetch("https://workspaceapi.herokuapp.com/api/space-type/")
   })
 });
 
+// search for available houly spaces
+bookingTypes.addEventListener('change', e => {
+  if (e.target.value == "hourly") {
+    
+    let endpoint = ("http://127.0.0.1:8000/api/available"+'/'+dateSearch);
+
+    let opt = document.createElement("option");
+    opt.textContent = "Select space";
+    opt.setAttribute("disabled", "disabled")
+    opt.setAttribute("selected", "selected")
+    spaces.replaceChildren(opt)
+
+    fetch(endpoint)
+    .then(response => response.json())
+    .then(data => {
+      data.forEach(d => {
+        let opt = document.createElement("option");
+        opt.value = d.id;
+        opt.textContent = d.space_name;
+        spaces.appendChild(opt);
+      });
+
+    startTime.removeAttribute("disabled");
+    spaceTypes.setAttribute("disabled", "disabled");
+    form.setAttribute("action", "http://127.0.0.1:8000/api/hourly-booking/new");
+
+    });
+
+  } else {
+    startTime.setAttribute("disabled", "disabled");
+    spaceTypes.removeAttribute("disabled");
+    form.setAttribute("action", "http://127.0.0.1:8000/api/bookings/new");
+  }
+  
+});
+
+
 // search for available spaces
 spaceTypes.addEventListener('change', e => {
-  console.log(dateSearch.toString());
-  let endpoint = ("https://workspaceapi.herokuapp.com/api/available"+'/'+dateSearch+'/'+e.target.value);
+  let endpoint = ("http://127.0.0.1:8000/api/available"+'/'+dateSearch+'/'+e.target.value);
 
   let opt = document.createElement("option");
   opt.textContent = "Select space";
@@ -36,7 +73,6 @@ spaceTypes.addEventListener('change', e => {
   fetch(endpoint)
   .then(response => response.json())
   .then(data => {
-    console.log(data);
     data.forEach(d => {
       let opt = document.createElement("option");
       opt.value = d.id;
@@ -49,7 +85,8 @@ spaceTypes.addEventListener('change', e => {
 
 dateSelector.addEventListener('change', e => {
   dateSearch = e.target.value;
-  spaceTypes.removeAttribute("disabled")
+
+  bookingTypes.removeAttribute("disabled");
 
   arr.push(e.target.value);
   dates.value = arr;
